2767420bd3a71e70774e339751d6dda71cb93d31
theseus007 <2013001@iiitdmj.ac.in>
2017-03-28 16:48:04 +0530

    feat:Init project

---
+++ app/src/main/java/com/example/theseus/interviewprep/Constants.java
@@ -0,0 +1,9 @@
+package com.example.theseus.interviewprep;
+
+/**
+ * Created by theseus on 28/3/17.
+ */
+
+public class Constants {
+public static final String DEMO_SERVICE_ACTION="com.example.theseus.interviewprep.DEMO_SERVICE";
+}
